ddd447c7af7bf71044447c3d1e99a8d36cb6a5b3
PeerM1204 <159874446+PeerM1204@users.noreply.github.com>
2024-03-03 23:27:54 +0500

    modules

---
+++ app.js
@@ -0,0 +1,7 @@
+import inquirer from "inquirer";
+let answers = await inquirer.prompt({
+    name: "age",
+    type: "number",
+    message: "Enter Your age"
+});
+console.log("Insha Allah, in " + (60 - answers.age) + " Years, You will be 60 Years old.");
